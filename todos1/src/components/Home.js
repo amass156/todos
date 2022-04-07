663b177eebfe055e80b2eaa2c9a3d7249a6164ba
amass156 <massiah156@gmail.com>
2022-04-06 21:09:43 -0400

    First commit

---
+++ todos1/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+       <h2>Andre's Todo App</h2>
+       <button>Add New</button>
+
+
+    </div>
+  )
+}
+
+export default Home
